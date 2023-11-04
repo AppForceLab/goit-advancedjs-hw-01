d39c2b1a407f1ab7a8b0f9def1ff1d0b792041fb
Volodymyr Popov <appforcelab@gmail.com>
2023-11-04 20:22:06 +0000

    add feedback js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash/throttle';
 
+const feedbackForm = document.querySelector('.feedback-form');
+const emailInput = feedbackForm.querySelector('input[name="email"]');
+const messageTextarea = feedbackForm.querySelector('textarea[name="message"]');
+const submitButton = feedbackForm.querySelector('button[type="submit"]');
+
+const updateLocalStorage = throttle(function () {
+  const feedbackState = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(feedbackState));
+}, 500);
+
+emailInput.addEventListener('input', updateLocalStorage);
+messageTextarea.addEventListener('input', updateLocalStorage);
+
+const storedFeedbackState = JSON.parse(
+  localStorage.getItem('feedback-form-state')
+);
+
+if (storedFeedbackState) {
+  emailInput.value = storedFeedbackState.email;
+  messageTextarea.value = storedFeedbackState.message;
+}
+
+feedbackForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  localStorage.removeItem('feedback-form-state');
+
+  const feedbackData = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+  console.log(feedbackData);
+
+  emailInput.value = '';
+  messageTextarea.value = '';
+});
