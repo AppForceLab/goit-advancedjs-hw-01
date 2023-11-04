c2ec9c492e5ed437065085424c49131f3b01fa7c
Volodymyr Popov <appforcelab@gmail.com>
2023-11-04 20:03:29 +0000

    add video player time key

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash/throttle';
 
+const vimeoPlayer = new Vimeo('vimeo-player');
+const currentTimeKey = 'videoplayer-current-time';
+
+vimeoPlayer.on('timeupdate', throttle(updateCurrentTime, 1000));
+
+function updateCurrentTime(event) {
+  const currentTime = event.seconds;
+  localStorage.setItem(currentTimeKey, currentTime);
+}
+
+window.addEventListener('load', () => {
+  const storedCurrentTime = localStorage.getItem(currentTimeKey);
+  if (storedCurrentTime) {
+    vimeoPlayer.setCurrentTime(parseFloat(storedCurrentTime));
+  }
+});
